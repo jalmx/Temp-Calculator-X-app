7e3e86bb295b96ca710c6816caffaa3c367af83b
Josef Leyva <jalm_x@hotmail.com>
2017-11-09 14:51:00 -0600

    UI ok

--- app/src/main/java/xizuth/com/tempcalcux/MainActivity.java
+++ app/src/main/java/xizuth/com/tempcalcux/MainActivity.java
@@ -1,7 +1,11 @@
 package xizuth.com.tempcalcux;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String[] s = getResources().getStringArray(R.array.temperature_selection);
     }
 }
